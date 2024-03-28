8a25da43c1547bbd6927ac7066e05956d6404682
minoru-yamanaka <minoruyamanaka@icloud.com>
2024-03-28 11:33:00 -0300

    Up280324-11:32 exe com var

---
+++ Aula_02/variaveis.js
@@ -0,0 +1,38 @@
+var pet ="Gato"; 
+var raca = "Persa";
+// Str 
+
+doguinho = "Caramelo";
+// declarar str < não declarar desta forma >
+
+const idadePet = 5;
+// number 
+
+// São Case Sensitive usaremos < camelCase, snake_case, kebab-case, PascalCase >
+
+// backtick -> crase < `` >
+
+// EsLint -> analisar code 
+
+// https://www.gitpoet.dev/ -> IA commit 
+
+let professor = "Thiago";
+console.log(`O nome do professor é ${professor} ele tem um ${pet} que é ${raca} e este tem ${idadePet} anos.` )
+
+// alt + [  cima ou baixo ] = movimentar a linha 
+// Shift + alt =  
+// Ctrl + L ou clear ou cls = limpar terminal 
+
+// const pet = "cachorro" // <- não posso converter de var para const 
+
+pet = "cachorro"
+
+console.log(`O nome do professor é ${professor} ele tem um ${pet} que é ${raca} e este tem ${idadePet} anos.` )
+
+// Corrigindo a lógica para verificar se o pet está vacinado
+// INSERE SE ESTÁ VACINADO  
+let vacinado = true;
+let mensagem = vacinado ? "Ele está vacinado." : "Ele não está vacinado.";
+console.log(mensagem);
+
+// Observação: as variáveis vacinadoYes e vacinadoNo não são necessárias
