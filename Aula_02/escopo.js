97230b8d2f382f72a7def21a8749257bc06151ee
minoru-yamanaka <minoruyamanaka@icloud.com>
2024-03-28 11:47:32 -0300

    Up280324-11:47

---
+++ Aula_02/escopo.js
@@ -0,0 +1,18 @@
+if(true){
+    //acessa code 
+    let pet = `Gato`;
+    console.log(`O(a) Pet ${pet}`);
+} 
+
+pet = "Cachorro";
+console.log(`O(a) Pet agora é ${pet}`);
+
+if (1>0){
+    let raca = `Persa`
+    console.log(`O(a) ${pet} é ${raca} `)
+}
+
+// var implicito 
+raca = `Rajado`
+console.log(`O(a) ${pet} é ${raca} `)
+
